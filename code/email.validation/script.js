c4f6185b1bd1473012b420a30e923c9d4fbc903d
to-xic09 <negi945896@gmail.com>
2026-04-23 16:53:55 +0530

    update js

---
+++ code/email.validation/script.js
@@ -0,0 +1,8 @@
+let email = document.querySelector("#email")
+let pass = document.querySelector("#password")
+let form = document.querySelector("form")
+
+form.addEventListener("submit",function (dets) {
+    dets.preventDefault();  
+    
+})
