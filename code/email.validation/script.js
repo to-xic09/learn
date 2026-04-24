3bda7031ba185bfbcd9cf4d1c194fcad59fcdea6
to-xic09 <negi945896@gmail.com>
2026-04-24 21:03:30 +0530

    update email validation code

--- code/email.validation/script.js
+++ code/email.validation/script.js
@@ -1,8 +1,33 @@
 let email = document.querySelector("#email")
-let pass = document.querySelector("#password")
-let form = document.querySelector("form")
+let password = document.querySelector("#password")
 
-form.addEventListener("submit",function (dets) {
-    dets.preventDefault();  
-    
+let form = document.querySelector("#u")
+
+form.addEventListener("submit", function (dets) {
+    dets.preventDefault();
+    document.querySelector("#emailError").textContent = "";
+    document.querySelector("#passwordError").textContent = "";
+
+
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/;
+    const passwordRegex =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/;
+
+
+    let emailValid = emailRegex.test(email.value);
+    let passwordValid = passwordRegex.test(password.value);
+
+
+    if (!emailValid) {
+        document.querySelector("#emailError").textContent = "Invalid Email ";
+        document.querySelector("#emailError").style.display = "initial";
+    }
+
+    if (!passwordValid) {
+        document.querySelector("#passwordError").textContent = "invaild password";
+       document.querySelector("#passwordError").style.display = "initial";
+    }
+
+
+
 })
